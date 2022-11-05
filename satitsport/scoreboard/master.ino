b11c7de5fd0af59f87e262e17a3825c1e37e8bb3
NETtoSan <netzamora05@gmail.com>
2022-11-05 13:13:23 +0700

    reformat code

--- satitsport/scoreboard/master.ino
+++ satitsport/scoreboard/master.ino
@@ -1,10 +1,10 @@
 /*
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
-  
+
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 */
@@ -18,7 +18,7 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include <WireSlave.h>
-int add1 = 34 ;
+int add1 = 34;
 int minus1 = 35;
 int clear1 = 32;
 int successPin = 33;
@@ -32,7 +32,8 @@ int scoreTeam2 = 0;
 uint8_t broadcastAddress[] = {0x30, 0xC6, 0xF7, 0x04, 0xD3, 0x00};
 // Structure example to send data
 // Must match the receiver structure
-typedef struct struct_message {
+typedef struct struct_message
+{
   int scoreA;
   int scoreB;
 } struct_message;
@@ -43,21 +44,23 @@ struct_message myData;
 esp_now_peer_info_t peerInfo;
 
 // callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
- 
-void setup() {
+void setup()
+{
   // Init Serial Monitor
   Serial.begin(115200);
- 
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
@@ -65,130 +68,135 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
-  
+
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
     Serial.println("Failed to add peer");
     return;
   }
   // pinMode pin that will be use to be inputs
-  pinMode(add1,INPUT);
+  pinMode(add1, INPUT);
   pinMode(minus1, INPUT);
-  pinMode(clear1,INPUT);
-  pinMode(successPin,OUTPUT);
-  pinMode(failedPin,OUTPUT);
+  pinMode(clear1, INPUT);
+  pinMode(successPin, OUTPUT);
+  pinMode(failedPin, OUTPUT);
 
   // Initialize I2C communications as Master
   WireSlave.begin()
-{
+  {
+  }
 
-}
- 
-void loop() {
-  Serial.println(digitalRead(add1));
+  void loop()
+  {
+    Serial.println(digitalRead(add1));
 
-  int add1Status = digitalRead(add1);
-  int minus1Status = digitalRead(minus1);
-  int clear1Status = digitalRead(clear1);
-  // Condition to send the message
-  if(add1Status == HIGH){
-    scoreTeam1 = scoreTeam1+1;
-    myData.scoreA = scoreTeam1;
-    myData.scoreB = 69;
-   // Send message via ESP-NOW
+    int add1Status = digitalRead(add1);
+    int minus1Status = digitalRead(minus1);
+    int clear1Status = digitalRead(clear1);
+    // Condition to send the message
+    if (add1Status == HIGH)
+    {
+      scoreTeam1 = scoreTeam1 + 1;
+      myData.scoreA = scoreTeam1;
+      myData.scoreB = 69;
+      // Send message via ESP-NOW
 
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
 
-    if (result == ESP_OK) {
-      digitalWrite(successPin,HIGH);
-      Serial.println("Sent with success YEHAHHHHHHH");
-      Serial.println(myData.scoreA);
-    }
-    else {
-      digitalWrite(failedPin,HIGH);
-      Serial.println("Error sending the data");
-    }
-    delay(200);
+      if (result == ESP_OK)
+      {
+        digitalWrite(successPin, HIGH);
+        Serial.println("Sent with success YEHAHHHHHHH");
+        Serial.println(myData.scoreA);
+      }
+      else
+      {
+        digitalWrite(failedPin, HIGH);
+        Serial.println("Error sending the data");
+      }
+      delay(200);
 
-    // Write a charatre to the Slave
-    WireSlave.beginTransmission(SLAVE_ADDR);
-    char teamAStr[5] ;
-    sprintf(teamAStr, '%d', scoreTeam1);
-    WireSlave.write("");
-    WireSlave.endTransmission();
+      // Write a charatre to the Slave
+      WireSlave.beginTransmission(SLAVE_ADDR);
+      char teamAStr[5];
+      sprintf(teamAStr, '%d', scoreTeam1);
+      WireSlave.write("");
+      WireSlave.endTransmission();
 
-    Serial.println("Receive data");
+      Serial.println("Receive data");
 
-    // Read response from Slave
-    // Read back 5 characters
-    WireSlave.requestFrom(SLAVE_ADDR,ANSWERSIZE);
+      // Read response from Slave
+      // Read back 5 characters
+      WireSlave.requestFrom(SLAVE_ADDR, ANSWERSIZE);
 
-    // Add characters to string
-    String response = "";
-    while (WireSlave.available()) {
+      // Add characters to string
+      String response = "";
+      while (WireSlave.available())
+      {
         char b = WireSlave.read();
         response += b;
-  } 
-  
-  // Print to Serial Monitor
-  Serial.println(response);
-  }
+      }
 
-  //minus button
-  if(minus1Status == HIGH){
-    scoreTeam1 = scoreTeam1 - 1;
-    myData.scoreA = scoreTeam1;
-    myData.scoreB = 69;
-   // Send message via ESP-NOW
-
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-    if (result == ESP_OK) {
-      digitalWrite(successPin,HIGH);
-      Serial.println("Sent with success YEHAHHHHHHH");
-      Serial.println(myData.scoreA);
+      // Print to Serial Monitor
+      Serial.println(response);
     }
-    else {
-      digitalWrite(failedPin,HIGH);
-      Serial.println("Error sending the data");
+
+    // minus button
+    if (minus1Status == HIGH)
+    {
+      scoreTeam1 = scoreTeam1 - 1;
+      myData.scoreA = scoreTeam1;
+      myData.scoreB = 69;
+      // Send message via ESP-NOW
+
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+      if (result == ESP_OK)
+      {
+        digitalWrite(successPin, HIGH);
+        Serial.println("Sent with success YEHAHHHHHHH");
+        Serial.println(myData.scoreA);
+      }
+      else
+      {
+        digitalWrite(failedPin, HIGH);
+        Serial.println("Error sending the data");
+      }
+      delay(200);
     }
+    if (clear1Status == HIGH)
+    {
+      scoreTeam1 = 0;
+      myData.scoreA = scoreTeam1;
+      myData.scoreB = 69;
+      // Send message via ESP-NOW
+
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+      if (result == ESP_OK)
+      {
+        digitalWrite(successPin, HIGH);
+        Serial.println("Sent with success YEHAHHHHHHH");
+        Serial.println(myData.scoreA);
+      }
+      else
+      {
+        digitalWrite(failedPin, HIGH);
+        Serial.println("Error sending the data");
+      }
+      delay(200);
+    }
+
+    // function to send the message, it's in void loop to prevent complicated issues if it's out of the void loop
+
+    digitalWrite(successPin, LOW);
+    digitalWrite(failedPin, LOW);
+
     delay(200);
   }
-  if(clear1Status == HIGH){
-    scoreTeam1 = 0;
-    myData.scoreA = scoreTeam1;
-    myData.scoreB = 69;
-   // Send message via ESP-NOW
-
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-    if (result == ESP_OK) {
-      digitalWrite(successPin,HIGH);
-      Serial.println("Sent with success YEHAHHHHHHH");
-      Serial.println(myData.scoreA);
-    }
-    else {
-      digitalWrite(failedPin,HIGH);
-      Serial.println("Error sending the data");
-    }
-    delay(200);
-  }
-
-  //function to send the message, it's in void loop to prevent complicated issues if it's out of the void loop
-  
-  digitalWrite(successPin,LOW);
-  digitalWrite(failedPin,LOW);
-
-  
-
-
-  
- delay(200);
-}
-
-
